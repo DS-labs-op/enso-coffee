35bfaba708bd70ebcd732cc9479eedaf12dda227
Divy Viradiya <divyviradiya2@gmail.com>
2025-12-14 20:19:17 +0530

    Remove Google Maps iframe from Contact page
    Removed embedded Google Maps iframe for Enso Coffee.

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -220,16 +220,7 @@ const Contact = () => {
 
               {/* Map */}
               <div className="h-64 md:h-80 rounded-xl overflow-hidden shadow-lg">
-                <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3719.8576!2d72.8234!3d21.2125!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2sEnso%20Coffee!5e0!3m2!1sen!2sin!4v1702561234567!5m2!1sen!2sin"
-                  width="100%"
-                  height="100%"
-                  style={{ border: 0 }}
-                  allowFullScreen
-                  loading="lazy"
-                  referrerPolicy="no-referrer-when-downgrade"
-                  title="Enso Coffee location on Google Maps"
-                />
+                
               </div>
 
               <a
